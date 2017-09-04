2c79829016d6daa1281674965c468f150efa189c
Vlad Soroceanu <vlad.soroceanu@kingfisher.com>
2017-09-04 12:14:47 +0100

    Switched from HapiJS to ExpressJS

--- app.js
+++ app.js
@@ -1,21 +1,92 @@
 'use strict'
 
-var Hapi = require('hapi')
-var port = process.env.PORT || 3000
+const debug = require('debug')('device-management-app:server'),
+  http = require('http'),
+  express = require('express'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  index = require('./routes/index'),
+  port = normalizePort(process.env.PORT || '3000')
 
-// Create a server with a host and port
-var server = new Hapi.Server()
-server.connection({
-    port: port
+
+// set up express app
+const app = express()
+app.set('port', port)
+
+// set up http server
+const server = http.createServer(app)
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  var addr = server.address()
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
+}
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// set static file folder
+app.use(express.static(path.join(__dirname, 'public')))
+// routes
+app.use('/', index)
+
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message)
 })
 
-// Add routes
-require('./routes')(server)
 
-// Start the server
-server.start((err) => {
-    if (err) {
-        throw err
-    }
-    console.log('Kingfisher Test Devices API running at:', server.info.uri)
-})
+// export module
+module.exports = app
+
+
+// PRIVATE FUNCTIONS
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10)
+  if (isNaN(port)) return val
+  if (port >= 0) return port
+  return false
+}
--- routes/index.js
+++ routes/index.js
@@ -1,171 +1,171 @@
 'use strict'
 
-const root = process.cwd() + '/',
-    fs = require('fs'),
-    config = require(root + 'config.json'),
-    deviceList = require(root + 'assets/devices.json')
+const express = require('express'),
+  router = express.Router(),
+  root = process.cwd() + '/',
+  fs = require('fs'),
+  config = require(root + 'config.json'),
+  deviceList = require(root + 'assets/devices.json')
 
-module.exports = function (server) {
-    // Add the route
-    server.route({
-        method: 'POST',
-        path: '/devices',
-        handler: (request, reply) => {
-            if (!request.payload || request.payload.token !== config.verificationToken) {
-                reply('Service temporarily unavailable').code(400)
-            } else {
-                let resp
-                switch (request.payload.text) {
-                    case '?':
-                        resp = '*We will display help regarding checkin-out and checking-in devices*'
-                        break
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' })
+})
 
-                    default:
-                        resp = createDeviceActionList(request.payload)
-                }
-                reply(resp)
-            }
+// devices route
+router.post('/devices', (req, res, next) => {
+  if (!req.body || req.body.token !== config.verificationToken) {
+    res.status(400).send({ test: 'Service temporarily unavailable' })
+  } else {
+    let resp
+    switch (req.body.text) {
+      case '?':
+        resp = '*We will display help regarding checkin-out and checking-in devices*'
+        break
+
+      default:
+        resp = createDeviceActionList(req.body)
+    }
+    res.send(resp)
+  }
+})
+
+// This route will answer button clicks
+router.post('/buttons', (req, res, next) => {
+  let payload = JSON.parse(req.body.payload)
+  if (!payload || payload.token !== config.verificationToken) {
+    res.status(400).send({ test: 'Service temporarily unavailable' })
+  } else {
+    let device = deviceList[payload.actions[0].value],
+      action = payload.actions[0].name,
+      resp = {
+        mrkdwn: true
+      }
+
+    switch (action) {
+      case 'check_out':
+        // save current user  
+        device.currentCheckout = {
+          userId: payload.user.id,
+          username: payload.user.name,
+          outAt: new Date().getTime()
         }
-    })
+        // set reply
+        resp.text = `Device \`${device.name}\` has been booked.\nGo get it from the locker and *Take care of it!* :smile:`
+        resp.delete_original = true
+        break
 
-    // This route will answer button clicks
-    server.route({
-        method: 'POST',
-        path: '/buttons',
-        handler: (request, reply) => {
-            let payload = JSON.parse(request.payload.payload)
-            if (!payload || payload.token !== config.verificationToken) {
-                reply('Service temporarily unavailable').code(400)
-            } else {
-                let device = deviceList[payload.actions[0].value],
-                    action = payload.actions[0].name,
-                    resp = {
-                        mrkdwn: true
-                    }
-
-                switch (action) {
-                    case 'check_out':
-                        // save current user  
-                        device.currentCheckout = {
-                            userId: payload.user.id,
-                            username: payload.user.name,
-                            outAt: new Date().getTime()
-                        }
-                        // set reply
-                        resp.text = `Device \`${device.name}\` has been booked.\nGo get it from the locker and *Take care of it!* :smile:`
-                        resp.delete_original = true
-                        break
-
-                    case 'check_in':
-                        // save current user as the last user to check out the device
-                        device.lastCheckout = {
-                            userId: payload.user.id,
-                            username: payload.user.name,
-                            outAt: device.currentCheckout.outAt,
-                            inAt: new Date().getTime()
-                        }
-                        // delete current user record
-                        delete device.currentCheckout
-                        // set reply
-                        resp.text = `Device \`${device.name}\` has been checked back in.\n*Please return the device to the locker.* Cheers!`
-                        resp.delete_original = true
-                        break
-
-                    default:
-                        let checkout = device.currentCheckout || device.lastCheckout,
-                            date = new Date(checkout.outAt).toUTCString()
-
-                        resp.text = `\`${device.name}\` was last checked out by *${checkout.username}* on *${date}*`
-                        // the following flag will tell Slack to not replace the printed list of devices with this message; the message will be
-                        // displayed underneath the list
-                        resp.replace_original = false
-                }
-                // write new device record to file
-                fs.writeFile(root + 'assets/devices.json', JSON.stringify(deviceList), 'utf8', () => { })
-                reply(resp)
-            }
+      case 'check_in':
+        // save current user as the last user to check out the device
+        device.lastCheckout = {
+          userId: payload.user.id,
+          username: payload.user.name,
+          outAt: device.currentCheckout.outAt,
+          inAt: new Date().getTime()
         }
-    })
-}
+        // delete current user record
+        delete device.currentCheckout
+        // set reply
+        resp.text = `Device \`${device.name}\` has been checked back in.\n*Please return the device to the locker.* Cheers!`
+        resp.delete_original = true
+        break
+
+      default:
+        let checkout = device.currentCheckout || device.lastCheckout,
+          date = new Date(checkout.outAt).toUTCString()
+
+        resp.text = `\`${device.name}\` was last checked out by *${checkout.username}* on *${date}*`
+        // the following flag will tell Slack to not replace the printed list of devices with this message; the message will be
+        // displayed underneath the list
+        resp.replace_original = false
+    }
+    // write new device record to file
+    fs.writeFile(root + 'assets/devices.json', JSON.stringify(deviceList), 'utf8', () => { })
+    res.send(resp)
+  }
+})
+
+module.exports = router
+
 
 // private functions
 function createDeviceActionList(payload) {
-    let availableCount = 0,
-        inUseCount = 0,
-        response = {
-            mrkdwn: true,
-            text: '*Below you can book or return any devices. Click on "Show Last Booking" to see who is the last person that booked the device.*',
-            attachments: [
-                {
-                    color: '#000000',
-                    mrkdwn_in: ["fields"],
-                    fields: [
-                        {
-                            title: 'Available',
-                            short: true
-                        },
-                        {
-                            title: 'In use',
-                            short: true
-                        }
-                    ]
-                }
-            ]
-        }
-
-    for (let a in deviceList) {
-        let device = deviceList[a],
-            attachment = {
-                callback_id: `device_${a}`,
-                attachment_type: 'default',
-                mrkdwn_in: ["text"],
-                title: device.name,
-                text: `*${device.type}, ${device.os}*\n${device.description}`,
-                thumb_url: device.thumbUrl,
-                actions: []
+  let availableCount = 0,
+    inUseCount = 0,
+    response = {
+      mrkdwn: true,
+      text: '*Below you can book or return any devices. Click on "Show Last Booking" to see who is the last person that booked the device.*',
+      attachments: [
+        {
+          color: '#000000',
+          mrkdwn_in: ["fields"],
+          fields: [
+            {
+              title: 'Available',
+              short: true
+            },
+            {
+              title: 'In use',
+              short: true
             }
-        // following condition will trigger if the device is checked out
-        if (device.currentCheckout) {
-            inUseCount++
-            // set attachment properties
-            attachment.color = 'warning'
-            // set an action for check in button
-            if (payload.user_id === device.currentCheckout.userId) {
-                attachment.actions = [{
-                    name: 'check_in',
-                    text: 'Return',
-                    type: 'button',
-                    style: 'primary',
-                    value: a
-                }]
-            } else {
-                let date = new Date(device.currentCheckout.outAt).toUTCString()
-                attachment.text += `\nThe device was booked by *${device.currentCheckout.username}* on *${date}*.`
-            }
+          ]
         }
-        // otherwise the device is available
-        else {
-            availableCount++
-            // set attachment properties
-            attachment.color = 'good'
-            attachment.actions = [{
-                name: "check_out",
-                text: "Book",
-                type: "button",
-                style: 'primary',
-                value: a
-            }, {
-                name: 'history',
-                text: 'Show Last Booking',
-                type: 'button',
-                value: a
-            }]
-        }
-        response.attachments.push(attachment)
+      ]
     }
 
-    // attach the avaible vs. inUse metrics
-    response.attachments[0].fields[0].value = availableCount
-    response.attachments[0].fields[1].value = inUseCount
-    return response
+  for (let a in deviceList) {
+    let device = deviceList[a],
+      attachment = {
+        callback_id: `device_${a}`,
+        attachment_type: 'default',
+        mrkdwn_in: ["text"],
+        title: device.name,
+        text: `*${device.type}, ${device.os}*\n${device.description}`,
+        thumb_url: device.thumbUrl,
+        actions: []
+      }
+    // following condition will trigger if the device is checked out
+    if (device.currentCheckout) {
+      inUseCount++
+      // set attachment properties
+      attachment.color = 'warning'
+      // set an action for check in button
+      if (payload.user_id === device.currentCheckout.userId) {
+        attachment.actions = [{
+          name: 'check_in',
+          text: 'Return',
+          type: 'button',
+          style: 'primary',
+          value: a
+        }]
+      } else {
+        let date = new Date(device.currentCheckout.outAt).toUTCString()
+        attachment.text += `\nThe device was booked by *${device.currentCheckout.username}* on *${date}*.`
+      }
+    }
+    // otherwise the device is available
+    else {
+      availableCount++
+      // set attachment properties
+      attachment.color = 'good'
+      attachment.actions = [{
+        name: "check_out",
+        text: "Book",
+        type: "button",
+        style: 'primary',
+        value: a
+      }, {
+        name: 'history',
+        text: 'Show Last Booking',
+        type: 'button',
+        value: a
+      }]
+    }
+    response.attachments.push(attachment)
+  }
+
+  // attach the avaible vs. inUse metrics
+  response.attachments[0].fields[0].value = availableCount
+  response.attachments[0].fields[1].value = inUseCount
+  return response
 }
